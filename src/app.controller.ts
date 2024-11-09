e1be56a7b95ea35e68f1b0bc6192678e04b670ce
Yougenez <Yougenez>
2024-11-09 21:47:46 +0800

    Completed Assignment 2: Prime Number

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,4 +22,17 @@ export class AppController {
     return { sequence };
   }
 
+  @Get("assignment-2-prime-number/:number")
+  getPrimeNumber(@Param('number') number: string): { isPrime: boolean } {
+    const num1 = parseInt(number);
+
+    for (let num2 = 2; num2 <= Math.sqrt(num1); num2++) {
+      if (num1 % num2 === 0) {
+        return { isPrime : false }; 
+      }
+    }
+
+    return { isPrime : true }; 
+  }
+
 }
